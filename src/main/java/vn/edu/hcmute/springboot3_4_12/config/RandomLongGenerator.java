87aea5bccfa7c57bdf10c8fe3d4ab3064bc7bc82
ryder3005 <ctpt3005@gmail.com>
2026-01-04 18:20:30 +0700

    add initial classes for RandomLongGenerator, MomoCallbackController, MomoPaymentRequest, and MomoService

---
+++ src/main/java/vn/edu/hcmute/springboot3_4_12/config/RandomLongGenerator.java
@@ -0,0 +1,4 @@
+package vn.edu.hcmute.springboot3_4_12.config;
+
+public class RandomLongGenerator {
+}
---
+++ src/main/java/vn/edu/hcmute/springboot3_4_12/dto/MomoPaymentRequest.java
@@ -0,0 +1,4 @@
+package vn.edu.hcmute.springboot3_4_12.dto;
+
+public class MomoPaymentRequest {
+}
